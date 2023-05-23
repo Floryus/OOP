6e5ffcf7df3998e8a682c4364ac0efd278d04eda
Alexander <88143826+xxxylan@users.noreply.github.com>
2023-05-23 12:14:34 +0200

    Neue Klasse "Adress", Erweiterung Klasse "Building"
    Neue Klasse "Address" für Adressangabe des Gebäudes
    Erweiterung Klasse "Building" mit neuen Eigenschaften -> Mitarbeiteranzahl, Besitzer, Baujahr, Parkplätze

---
+++ Facility Management/src/classes/Address.java
@@ -0,0 +1,52 @@
+package classes;
+
+public class Address {
+    private String street;
+    private int houseNumber;
+    private int postalCode;
+    private String city;
+
+    public Address(String street, int houseNumber, int postalCode, String city) {
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return houseNumber + " " + street + ", " + postalCode + " " + city;
+    }
+}
